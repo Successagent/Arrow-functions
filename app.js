9c3c2e525654c75db6fb77b8a14f6e855cf6f366
successAgent <miesineagent@gmail.com>
2022-08-08 11:11:43 +0100

    arrow-functions

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+ * Arrow functions can have arguments too
+
+* If an arrow functions has a single parameter, the parentheses enclosing it can be remove
+
+*if the parentheses enclosing the function are removed you can remove the curly braces enclosing the function body and the return statement
+
+* For you to write an arrow function, you will start with const and the name of the function then the block of the function
+*/
+
+const arrowFunc = () => 123;  // Arrow functions
